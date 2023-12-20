0f98f4b9ebb8364fcb9f55c51ea5dbce8ad66c6e
Anlimah <y.m.ratty7@gmail.com>
2023-12-20 13:07:05 +0000

    working on creating admission letters 3

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1734,8 +1734,8 @@ class AdminController
         try {
             // Load the Word document
             $phpWordObj = \PhpOffice\PhpWord\IOFactory::createReader("Word2007");
-            $templatePath = __DIR__ . DIRECTORY_SEPARATOR . 'admission_letters' . DIRECTORY_SEPARATOR . 'letter_template.docx';
-            $phpWord = $phpWordObj->load($templatePath);
+            //$templatePath = __DIR__ . DIRECTORY_SEPARATOR . 'admission_letters' . DIRECTORY_SEPARATOR . 'letter_template.docx';
+            $phpWord = $phpWordObj->load(__DIR__ . '/admission_letters/letter_template.docx');
 
             // Replace placeholders with actual data
             $phpWord->setValue('Full_Name', "Francis Anlimah");
