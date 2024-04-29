43c409dc6f219794a5276d0e2f7ea91065daa650
Anlimah <y.m.ratty7@gmail.com>
2024-04-29 16:56:42 -0700

    Complited email submission format for admissions and enrollements 5

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2378,16 +2378,16 @@ class AdminController
         };
 
         $dur_word = match ($data["pi"][0]["duration"]) {
-            1 => 'One',
-            2 => 'Two',
-            3 => 'Three',
-            4 => 'Four',
-            5 => 'Five',
-            6 => 'Six',
-            7 => 'Seven',
-            8 => 'Eight',
-            9 => 'Nine',
-            10 => 'Ten'
+            '1' => 'One',
+            '2' => 'Two',
+            '3' => 'Three',
+            '4' => 'Four',
+            '5' => 'Five',
+            '6' => 'Six',
+            '7' => 'Seven',
+            '8' => 'Eight',
+            '9' => 'Nine',
+            '10' => 'Ten'
         };
 
         $email = $data["email_addr"];
